0abc44aed53b311a1ca8195d4ffd527d89f7bd38
hajieew <hajieew@gmail.com>
2026-05-26 22:41:07 +0400

    Salim

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Module for executing multiple tasks concurrently"""
+
+import asyncio
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[int]:
+    """Spawns task_wait_random n times and returns delays in ascending order"""
+
+    tasks = [task_wait_random(max_delay) for i in range(n)]
+    delays = []
+
+    for task in asyncio.as_completed(tasks):
+        result = await task
+        delays.append(result)
+
+    return delays
