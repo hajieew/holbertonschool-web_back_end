a7064c3e4a68fd1edd5646682b2edf83cd3f47ea
hajieew <hajieew@gmail.com>
2026-05-26 23:13:05 +0400

    Salim

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module for async comprehension"""
+
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[int]:
+    """Collects 10 random numbers using async comprehension"""
+
+    return [i async for i in async_generator()]
