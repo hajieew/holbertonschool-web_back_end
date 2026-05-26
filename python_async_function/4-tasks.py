9a02a51a6f6d31ac827846e5a1df7969a7360018
hajieew <hajieew@gmail.com>
2026-05-26 22:42:06 +0400

    Salim

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -7,7 +7,7 @@ from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> List[int]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Spawns task_wait_random n times and returns delays in ascending order"""
 
     tasks = [task_wait_random(max_delay) for i in range(n)]
