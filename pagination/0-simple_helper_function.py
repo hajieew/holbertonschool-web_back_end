0307eff56d317d9266c96d9870c9e9c6a11d8de0
hajieew <hajieew@gmail.com>
2026-06-25 21:35:59 +0400

    Salim

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""aloaloalo"""
+
+
+def index_range(page, page_size) -> tuple[int, int]:
+    """alooooooooooooooooooooooooooo"""
+
+    start_page = (page - 1) * page_size
+    end_page = start_page + page_size
+    return (start_page, end_page)
