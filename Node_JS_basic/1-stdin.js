5c92a9350da088acea30d2314b1db77e3ee73de1
hajieew <hajieew@gmail.com>
2026-07-21 20:33:59 +0400

    Salim

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,9 +1,11 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (name) => {
-  console.log(`Your name is: ${name.toString().trim()}`);
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data.trim()}\n`);
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
